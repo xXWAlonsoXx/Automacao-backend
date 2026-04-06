7cf9c4edc766bb907d8d91c8fb42a5ca994bd07f
xXWAlonsoXx <164101278+xXWAlonsoXx@users.noreply.github.com>
2026-04-06 16:34:23 -0300

    Create firebaseAdmin.js

---
+++ src/config/firebaseAdmin.js
@@ -0,0 +1,11 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
